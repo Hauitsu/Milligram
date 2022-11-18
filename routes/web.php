e1491e06ef4f021854fb9aeaf9d8c82f3940b87e
Hauitsu <68635159+Hauitsu@users.noreply.github.com>
2022-11-18 21:15:42 +0700

    0.1
    Welcome Cover, Login, Register, Dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/dbconn', function () {
-    return view('dbconn');
-});
-Route::get('/cover', function () {
-    return view('cover');
-});
-Route::get('/login', function () {
-    return view('login');
-});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
